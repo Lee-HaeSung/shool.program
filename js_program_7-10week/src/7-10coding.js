7a824ee7bf9355a99ec569c8df04aaca46ceeebe
Lee-HaeSung <haesung116@naver.com>
2025-04-14 21:49:24 +0900

    week7_1

--- js_program_7-10week/src/7-10coding.js
+++ js_program_7-10week/src/7-10coding.js
@@ -14,13 +14,37 @@
 //2. 가장 큰 값을 출력 하세요
 //3. 짝수인 요소만 배열로 출력하세요
 
-let arr1 = [36, 122, 79, 8, "무궁화", 391, "words", 509, 20192, "B", 321, 473, 73, "france", "Korea", 24, 99];
-//1번
-arr1.sort(function(a,b) {return a - b}); //오름차순
-console.log(arr1);
-//2번
-arr1.sort(function(a,b) {return b - a}); //내림차순
-console.log(arr1[0]);
-//3번
+
+//// 입력받은 높이의 피라미드 만들기
+//let n = prompt("입력");
+//let p = " ";
+//let q = "*";
+//let z = 1;
+//for(let i = 0; i < n; i++) {
+//    console.log(p.repeat(n-(i+1)) + q.repeat(z) + p.repeat(n-(i+1)));
+//    z += 2;
+//    }
 
 
+//// 로또 만들기
+////1~45번 까지의 숫자 5개, 보너스 1개를 랜덤으로 만든다.
+////숫자 6개가 저장되는 배열을 선언하여 [0, 0, 0, 0, 0, 0] 오름차순으로 정렬한다.
+////math.random() * math.ceil() 중복된 숫자는 제거한다.
+//let randomPick = [];
+//for (let i = 0; i < 6; i++){
+//    randomNum = Math.floor(Math.random()*(45-1)+1);
+//    if (randomPick.indexOf(randomNum) === -1) {
+//        randomPick.push(randomNum);
+//    } else {
+//        i--;
+//    }
+//}
+//
+//console.log(randomPick[0] + ", " + randomPick[1] + ", " + randomPick[2] + ", " + randomPick[3] + ", " + randomPick[4] + "은 랜덤 숫자이고 " + randomPick[5] + "은 보너스입니다.");
+//randomPick.sort(function (a, b) {
+//    return a-b;
+//});
+//console.log(randomPick);
+
+
+//
