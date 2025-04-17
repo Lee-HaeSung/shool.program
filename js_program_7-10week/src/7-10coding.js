b8fe0ec7078c17aaa1e97039145eef8e734f75e4
Lee-HaeSung <haesung116@naver.com>
2025-04-17 18:49:04 +0900

    week7_4.17_cancle

--- js_program_7-10week/src/7-10coding.js
+++ js_program_7-10week/src/7-10coding.js
@@ -205,40 +205,60 @@
 
 //27-2번: 객체 만들기
 
-class Exam27 {
-    maps = new Map();
+//class Exam27 {
+//    maps = new Map();
+//
+//    inputFunc() {
+//        let names = prompt("이름을 공백으로 구분하여 입력하세요.");
+//        let score = prompt("점수를 공백으로 구분하여 입력하세요.");
+//        let arrName = names.split(" ");
+//        let arrScore = score.split(" ");
+//        if (arrName.length != arrScore.length) {
+//            alert("두개 배열의 길이가 다릅니다. 다시 시작하세요.");
+//            return false;   // 함수가 false 를 리턴한다.
+//        }
+//
+//        for(let i = 0; i < arrName.length; i++){
+//        this.maps.set(arrName[i], Number(arrScore[i])); // Number 는 문자열을 숫자로 변경
+//        }
+//        console.log(this.maps);
+//        return true;    // 함수가 true 를 리턴한다.
+//    }
+//
+//    outputFunc() {
+//        let strOutput = "{";
+//        for( let item of this.maps ) {
+//            console.log(item);
+//            strOutput += `'${item[0]}': ${item[1]}, `
+//        }
+//        strOutput = strOutput.substring(0 , strOutput.length - 2);
+//        // 문자열의 길이에서 -2 시킨 길이의 문자열로 다시 저장. "abcd" => "ab"
+//        strOutput += "}";
+//        console.log(strOutput);
+//    }
+//}
+//
+//let exam = new Exam27();
+//if ( exam.inputFunc() ) {   // inputFunc() 를 실행하고 리턴값이 true 이면
+//    exam.outputFunc();  // outputFunc() 를 실행한다.
+//}
 
-    inputFunc() {
-        let names = prompt("이름을 공백으로 구분하여 입력하세요.");
-        let score = prompt("점수를 공백으로 구분하여 입력하세요.");
-        let arrName = names.split(" ");
-        let arrScore = score.split(" ");
-        if (arrName.length != arrScore.length) {
-            alert("두개 배열의 길이가 다릅니다. 다시 시작하세요.");
-            return false;   // 함수가 false 를 리턴한다.
-        }
+//28번 : 2-gram
+class Ex28 {
+    strInput = ""; //문자열 초기화 및 생성
 
-        for(let i = 0; i < arrName.length; i++){
-        this.maps.set(arrName[i], Number(arrScore[i])); // Number 는 문자열을 숫자로 변경
-        }
-        console.log(this.maps);
-        return true;    // 함수가 true 를 리턴한다.
+    constructor(str){
+        this.strInput = str;
     }
 
-    outputFunc() {
-        let strOutput = "{";
-        for( let item of this.maps ) {
-            console.log(item);
-            strOutput += `'${item[0]}': ${item[1]}, `
+    output(){
+        // 루프를 이용하여 문자열 2글자씩 출력한다.
+        for(let i = 0; i < this.strInput.length-1 ; i++){
+        console.log(this.strInput.charAt(i) + " " + this.strInput.charAt(i+1)); //charAt()가 문자열 받기
         }
-        strOutput = strOutput.substring(0 , strOutput.length - 2);
-        // 문자열의 길이에서 -2 시킨 길이의 문자열로 다시 저장. "abcd" => "ab"
-        strOutput += "}";
-        console.log(strOutput);
     }
-}
 
-let exam = new Exam27();
-if ( exam.inputFunc() ) {   // inputFunc() 를 실행하고 리턴값이 true 이면
-    exam.outputFunc();  // outputFunc() 를 실행한다.
 }
+let exam28 = new Ex28(prompt("문자열을 입력하세요."));
+exam28.output();
+
