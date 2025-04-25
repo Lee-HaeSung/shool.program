986eed42f2927e689f51c3f2a22f9df3a7f1b542
Lee-HaeSung <haesung116@naver.com>
2025-04-25 17:16:53 +0900

    111

--- js_program_7-10week/src/7-10coding.js
+++ js_program_7-10week/src/7-10coding.js
@@ -243,22 +243,89 @@
 //    exam.outputFunc();  // outputFunc() 를 실행한다.
 //}
 
-//28번 : 2-gram
-class Ex28 {
-    strInput = ""; //문자열 초기화 및 생성
+////28번 : 2-gram
+//class Ex28 {
+//    strInput = ""; //문자열 초기화 및 생성
+//
+//    constructor(str){
+//        this.strInput = str;
+//    }
+//
+//    output(){
+//        // 루프를 이용하여 문자열 2글자씩 출력한다.
+//        for(let i = 0; i < this.strInput.length-1 ; i++){
+//        console.log(this.strInput.charAt(i) + " " + this.strInput.charAt(i+1)); //charAt()가 문자열 받기
+//        }
+//    }
+//
+//}
+//let exam28 = new Ex28(prompt("문자열을 입력하세요."));
+//exam28.output();
 
-    constructor(str){
-        this.strInput = str;
-    }
+    //37번 : 반장 선거
+    //class Exam37 {
+    //    arrItem = [];
+    //    his.arrItem = input.split(" ");
+    //    }
+    //    checkVotes() {
+    //
+    //        for( let i = 0; i < this.arrItem.length; i++ ) {
+    //            if ( this.isExistItem(this.arrItem[i]) ) {
+    //                // map 에 원소이름의 키가 있을때 실행한다.
+    //                let number = this.maps.get(this.arrItem[i]);
+    //                // map 에서 원소이름이 키인 원래값을 가져온다.
+    //                this.maps.set(this.arrItem[i], number + 1);
+    //                // map 에 {원소이름: 원래값+1} 으로 저장 한다
+    //                // this.maps.set( 키, 값 );
+    //            } else {
+    //                // map 에 원소이름의 키가 없을때 실행한다.
+    //                this.maps.set(this.arrItem[i], 1);
+    //                // map 에 {원소이름: 1} 으로 저장 한다
+    //                // this.maps.set( 키, 값 );
+    //            }
+    //        }
+    //        console.log(this.maps);
+    //        console.log([...this.maps]);    // [... JavaScript Map, Set, Array] ===> 배열로 변환
+    //        let item = [...this.maps].reduce( (a,b) =>  // Array reduce 사용
+    //            a[1] >= b[1] ? a : b    // 앞의 원소 a[1] 값 >= 뒤의 원소 b[1] 값 보다 크면 a 를 리턴
+    //        );
+    //        return item;
+    //        // return 가장 [1] 값이 큰 map 원소를 리턴한다.
+    //    }
+    //    isExistItem(item) {
+    //        // 원소가 maps 에 키로 존재하면 true 를 리턴한다.
+    //        return this.maps.has(item);
+    //        // if ( this.maps.has(item) == true )
+    //    }
+    //}
+    //// 원범 혜원 유빈 원범 혜원 곰 유빈 원범 곰 혜원 호랑이 혜원
+    //let exam37 = new Exam37(prompt("투표할 항목을 공백으로 구분해서 입력하세요."));
+    //let result = exam37.checkVotes();
+    //console.log(`투표 결과 ${result[0]가 ${result[1]}표로 가장 많습니다.}`);
+    //console.log(`투표 결과 ${result[0]}가 ${result[1]}표로 가장 많습니다.`);
 
-    output(){
-        // 루프를 이용하여 문자열 2글자씩 출력한다.
-        for(let i = 0; i < this.strInput.length-1 ; i++){
-        console.log(this.strInput.charAt(i) + " " + this.strInput.charAt(i+1)); //charAt()가 문자열 받기
-        }
-    }
-
-}
-let exam28 = new Ex28(prompt("문자열을 입력하세요."));
-exam28.output();
-
+////41번 : 소수판별
+//class Ex41 {
+//    constructor(){
+//    this.Num41 = 0;
+//    }
+//
+//    primeNumber() {
+//        let input = prompt("소수 판별할 숫자를 입력해주세요");
+//        let input2 = Number(input);
+//        let add = 0;
+//
+//        for(let i = 2; i < input2; i++){
+//            if(input2 % i == 0){
+//                add ++;
+//            }
+//        }
+//        if (add == 0)
+//        console.log("소수입니다.");
+//
+//        else
+//        console.log("소수가 아닙니다.");
+//    }
+//}
+//let Exam41 = new Ex41();
+//Exam41.primeNumber();
